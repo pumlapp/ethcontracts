9d5406bc9369057ab8d8a312ccd1e85766e34f90
nickjjang <nickjang234@gmail.com>
2021-11-19 08:53:53 -0700

    fixed automaticSetWinner

--- contracts/Engine.sol
+++ contracts/Engine.sol
@@ -6,8 +6,9 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./PumlNFT.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Engine is Ownable {
+contract Engine is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
 
     // Event triggered when an auction is created
@@ -25,52 +26,57 @@ contract Engine is Ownable {
     event PaymentToOwner(
         address receiver,
         uint256 amount,
-        uint256 paidByCustomer,
+        //    uint256 paidByCustomer,
         uint256 commission,
         uint256 royalties,
         uint256 safetyCheckValue
     );
 
     // Status of an auction, calculated using the start date, the duration and the current timeblock
-    enum Status {pending, active, finished}
+    enum Status {
+        pending,
+        active,
+        finished
+    }
     // Data of an auction
     struct Auction {
-        address assetAddress;               // token address
-        uint256 assetId;                    // token id
-        address payable creator;            // creator of the auction, which is the token owner
-        uint256 startTime;                  // time (unix, in seconds) where the auction will start
-        uint256 duration;                   // duration in seconds of the auction
-        uint256 currentBidAmount;           // amount in ETH of the current bid amount
-        address payable currentBidOwner;    // address of the user who places the best bid
-        uint256 bidCount;                   // number of bids of the auction
+        address assetAddress; // token address
+        uint256 assetId; // token id
+        address payable creator; // creator of the auction, which is the token owner
+        uint256 startTime; // time (unix, in seconds) where the auction will start
+        uint256 duration; // duration in seconds of the auction
+        uint256 currentBidAmount; // amount in ETH of the current bid amount
+        address payable currentBidOwner; // address of the user who places the best bid
+        uint256 bidCount; // number of bids of the auction
     }
     Auction[] public auctions;
 
     uint256 public commission = 0; // this is the commission in basic points that will charge the marketplace by default.
     uint256 public accumulatedCommission = 0; // this is the amount in ETH accumulated on marketplace wallet
+    uint256 public totalSales = 0;
 
     struct Offer {
-        address assetAddress;       // address of the token
-        uint256 tokenId;            // the tokenId returned when calling "createItem"
-        address payable creator;    // who creates the offer
-        uint256 price;              // price of each token
-        bool isOnSale;              // is on sale or not
-        bool isAuction;             // is this offer is for an auction
-        uint256 idAuction;          // the id of the auction
+        address assetAddress; // address of the token
+        uint256 tokenId; // the tokenId returned when calling "createItem"
+        address payable creator; // who creates the offer
+        uint256 price; // price of each token
+        bool isOnSale; // is on sale or not
+        bool isAuction; // is this offer is for an auction
+        uint256 idAuction; // the id of the auction
     }
     mapping(uint256 => Offer) public offers;
 
     // Every time a token is put on sale, an offer is created. An offer can be a direct sale, an auction
     // or a combination of both.
     function createOffer(
-        address _assetAddress,  // address of the token
-        uint256 _tokenId,       // tokenId
-        bool _isDirectSale,     // true if can be bought on a direct sale
-        bool _isAuction,        // true if can be bought in an auction
-        uint256 _price,         // price that if paid in a direct sale, transfers the NFT
-        uint256 _startPrice,    // minimum price on the auction
-        uint256 _startTime,     // time when the auction will start. Check the format with frontend
-        uint256 _duration       // duration in seconds of the auction
+        address _assetAddress, // address of the token
+        uint256 _tokenId, // tokenId
+        bool _isDirectSale, // true if can be bought on a direct sale
+        bool _isAuction, // true if can be bought in an auction
+        uint256 _price, // price that if paid in a direct sale, transfers the NFT
+        uint256 _startPrice, // minimum price on the auction
+        uint256 _startTime, // time when the auction will start. Check the format with frontend
+        uint256 _duration // duration in seconds of the auction
     ) public {
         ERC721 asset = ERC721(_assetAddress);
         require(asset.ownerOf(_tokenId) == msg.sender, "Not the owner");
@@ -78,17 +84,23 @@ contract Engine is Ownable {
             asset.getApproved(_tokenId) == address(this),
             "NFT not approved"
         );
+        // Could not be used to update an existing offer (#02)
+        Offer memory previous = offers[_tokenId];
+        require(
+            previous.isOnSale == false && previous.isAuction == false,
+            "An active offer already exists"
+        );
+
         // First create the offer
-        Offer memory offer =
-            Offer({
-                assetAddress: _assetAddress,
-                tokenId: _tokenId,
-                creator: payable(msg.sender),
-                price: _price,
-                isOnSale: _isDirectSale,
-                isAuction: _isAuction,
-                idAuction: 0
-            });
+        Offer memory offer = Offer({
+            assetAddress: _assetAddress,
+            tokenId: _tokenId,
+            creator: payable(msg.sender),
+            price: _price,
+            isOnSale: _isDirectSale,
+            isAuction: _isAuction,
+            idAuction: 0
+        });
         // only if the offer has the "is_auction" flag, add the auction to the list
         if (_isAuction) {
             offer.idAuction = createAuction(
@@ -134,6 +146,7 @@ contract Engine is Ownable {
 
     // Changes the default commission. Only the owner of the marketplace can do that. In basic points
     function setCommission(uint256 _commission) public onlyOwner {
+        require(_commission <= 5000, "Commission too high");
         commission = _commission;
     }
 
@@ -142,7 +155,7 @@ contract Engine is Ownable {
     // funds are transferred to the token owner.
     // is there is an auction open, the last bid amount is sent back to the last bidder
     // After that, the offer is cleared.
-    function buy(uint256 _tokenId) external payable {
+    function buy(uint256 _tokenId) external payable nonReentrant {
         address buyer = msg.sender;
         uint256 paidPrice = msg.value;
 
@@ -151,9 +164,17 @@ contract Engine is Ownable {
         uint256 price = offer.price;
         require(paidPrice >= price, "Price is not enough");
 
+        //if there is a bid and the auction is closed but not claimed, give priority to claim
+        require(
+            !(offer.isAuction == true &&
+                isFinished(offer.idAuction) &&
+                auctions[offer.idAuction].bidCount > 0),
+            "Claim asset from auction has priority"
+        );
+
         emit Claim(_tokenId, buyer);
         PumlNFT asset = PumlNFT(offer.assetAddress);
-        asset.transferFrom(offer.creator, buyer, _tokenId);
+        asset.safeTransferFrom(offer.creator, buyer, _tokenId);
 
         // now, pay the amount - commission - royalties to the auction creator
         address payable creatorNFT = payable(asset.getCreator(_tokenId));
@@ -162,17 +183,25 @@ contract Engine is Ownable {
         uint256 royaltiesToPay = 0;
         if (creatorNFT != offer.creator) {
             // It is a resale. Transfer royalties
-            royaltiesToPay = (paidPrice.mul(asset.getRoyalties(_tokenId))) / 10000;
-            creatorNFT.transfer(royaltiesToPay);
+            royaltiesToPay =
+                (paidPrice.mul(asset.getRoyalties(_tokenId))) /
+                10000;
+
+            (bool success, ) = creatorNFT.call{value: royaltiesToPay}("");
+            require(success, "Transfer failed.");
+
             emit Royalties(creatorNFT, royaltiesToPay);
         }
-        uint256 amountToPay = paidPrice.sub(commissionToPay).sub(royaltiesToPay);
+        uint256 amountToPay = paidPrice.sub(commissionToPay).sub(
+            royaltiesToPay
+        );
 
-        offer.creator.transfer(amountToPay);
+        (bool success2, ) = offer.creator.call{value: amountToPay}("");
+        require(success2, "Transfer failed.");
         emit PaymentToOwner(
             offer.creator,
-            amountToPay, 
-            paidPrice,
+            amountToPay,
+            //     paidPrice,
             commissionToPay,
             royaltiesToPay,
             amountToPay + ((paidPrice * commission) / 10000) // using safemath will trigger an error because of stack size
@@ -181,9 +210,13 @@ contract Engine is Ownable {
         // is there is an auction open, we have to give back the last bid amount to the last bidder
         if (offer.isAuction == true) {
             Auction memory auction = auctions[offer.idAuction];
-            if (auction.currentBidAmount != 0) {
+            // #4. Only if there is at least a bid and the bid amount > 0, give it back to last bidder
+            if (auction.currentBidAmount != 0 && auction.bidCount > 0) {
                 // return funds to the previuos bidder
-                auction.currentBidOwner.transfer(auction.currentBidAmount);
+                (bool success3, ) = auction.currentBidOwner.call{
+                    value: auction.currentBidAmount
+                }("");
+                require(success3, "Transfer failed.");
                 emit ReturnBidFunds(
                     offer.idAuction,
                     auction.currentBidOwner,
@@ -197,6 +230,8 @@ contract Engine is Ownable {
         offer.isAuction = false;
         offer.isOnSale = false;
         offers[_tokenId] = offer;
+
+        totalSales = totalSales.add(msg.value);
     }
 
     // Creates an auction for a token. It is linked to an offer
@@ -211,17 +246,16 @@ contract Engine is Ownable {
             _startTime = block.timestamp;
         }
 
-        Auction memory auction =
-            Auction({
-                creator: payable(msg.sender),
-                assetAddress: _assetAddress,
-                assetId: _assetId,
-                startTime: _startTime,
-                duration: _duration,
-                currentBidAmount: _startPrice,
-                currentBidOwner: payable(address(0)),
-                bidCount: 0
-            });
+        Auction memory auction = Auction({
+            creator: payable(msg.sender),
+            assetAddress: _assetAddress,
+            assetId: _assetId,
+            startTime: _startTime,
+            duration: _duration,
+            currentBidAmount: _startPrice,
+            currentBidOwner: payable(address(0)),
+            bidCount: 0
+        });
         auctions.push(auction);
         uint256 index = auctions.length.sub(1);
 
@@ -232,21 +266,25 @@ contract Engine is Ownable {
 
     // At the end of the call, the amount is saved on the marketplace wallet and the previous bid amount is returned to old bidder
     // except in the case of the first bid, as could exists a minimum price set by the creator as first bid.
-    function bid(uint256 auctionIndex) public payable {
+    function bid(uint256 auctionIndex) public payable nonReentrant {
         Auction storage auction = auctions[auctionIndex];
-        require(auction.creator != address(0));
-        require(isActive(auctionIndex));
+        require(auction.creator != address(0), "Cannot bid. Error in auction");
+        require(isActive(auctionIndex), "Bid not active");
         require(msg.value > auction.currentBidAmount, "Bid too low");
         // we got a better bid. Return funds to the previous best bidder
         // and register the sender as `currentBidOwner`
 
         // this check is for not transferring back funds on the first bid, as the fist bid is the minimum price set by the auction creator
+        // and the bid owner is address(0)
         if (
             auction.currentBidAmount != 0 &&
-            auction.currentBidOwner != auction.creator
+            auction.currentBidOwner != address(0)
         ) {
             // return funds to the previuos bidder
-            auction.currentBidOwner.transfer(auction.currentBidAmount);
+            (bool success, ) = auction.currentBidOwner.call{
+                value: auction.currentBidAmount
+            }("");
+            require(success, "Transfer failed.");
             emit ReturnBidFunds(
                 auctionIndex,
                 auction.currentBidOwner,
@@ -293,13 +331,19 @@ contract Engine is Ownable {
     }
 
     // returns the user with the best bid until now on an auction
-    function getCurrentBidOwner(uint256 _auctionIndex) public view returns (address)
+    function getCurrentBidOwner(uint256 _auctionIndex)
+        public
+        view
+        returns (address)
     {
         return auctions[_auctionIndex].currentBidOwner;
     }
 
     // returns the amount in ETH of the best bid until now on an auction
-    function getCurrentBidAmount(uint256 _auctionIndex) public view returns (uint256)
+    function getCurrentBidAmount(uint256 _auctionIndex)
+        public
+        view
+        returns (uint256)
     {
         return auctions[_auctionIndex].currentBidAmount;
     }
@@ -314,49 +358,75 @@ contract Engine is Ownable {
         require(isFinished(auctionIndex), "Auction not finished yet");
         return auctions[auctionIndex].currentBidOwner;
     }
- 
+
     // called when the auction is finished by the user who won the auction
     // transfer the nft to the caller, the royalties (if any) to the nft creator
     // the commission of the marketplace is calculated, and the remaining funds
     // are transferred to the token owner
     // After this, the offer is disabled
     function claimAsset(uint256 auctionIndex) public {
+        address winner = getWinner(auctionIndex);
+        require(winner == msg.sender, "You are not the winner of the auction");
+        auctionTransferAsset(auctionIndex);
+    }
+
+    function automaticSetWinner(uint256 auctionIndex) public onlyOwner {
+        auctionTransferAsset(auctionIndex);
+    }
+
+    function auctionTransferAsset(uint256 auctionIndex) private nonReentrant {
         require(isFinished(auctionIndex), "The auction is still active");
         Auction storage auction = auctions[auctionIndex];
 
         address winner = getWinner(auctionIndex);
-        require(winner == msg.sender, "You are not the winner of the auction");
 
         // the token could be sold in direct sale or the owner cancelled the auction
         Offer memory offer = offers[auction.assetId];
         require(offer.isAuction == true, "NFT not in auction");
 
         PumlNFT asset = PumlNFT(auction.assetAddress);
-        asset.transferFrom(auction.creator, winner, auction.assetId);
+
+        // #3, check if the asset owner had removed their approval or the offer creator is not the token owner anymore.
+        require(
+            asset.getApproved(auction.assetId) == address(this),
+            "NFT not approved"
+        );
+        require(
+            asset.ownerOf(auction.assetId) == auction.creator,
+            "Auction creator is not nft owner"
+        );
+
+        asset.safeTransferFrom(auction.creator, winner, auction.assetId);
 
         emit Claim(auctionIndex, winner);
 
         // now, pay the amount - commission - royalties to the auction creator
         address payable creatorNFT = payable(asset.getCreator(auction.assetId));
-        uint256 commissionToPay =
-            (auction.currentBidAmount.mul(commission)) / 10000;
+        uint256 commissionToPay = (auction.currentBidAmount.mul(commission)) /
+            10000;
         uint256 royaltiesToPay = 0;
         if (creatorNFT != auction.creator) {
             // It is a resale. Transfer royalties
             royaltiesToPay =
-                (auction.currentBidAmount.mul(asset.getRoyalties(auction.assetId))) /
+                (
+                    auction.currentBidAmount.mul(
+                        asset.getRoyalties(auction.assetId)
+                    )
+                ) /
                 10000;
             creatorNFT.transfer(royaltiesToPay);
             emit Royalties(creatorNFT, royaltiesToPay);
         }
-        uint256 amountToPay =
-            auction.currentBidAmount.sub(commissionToPay).sub(royaltiesToPay);
+        uint256 amountToPay = auction.currentBidAmount.sub(commissionToPay).sub(
+            royaltiesToPay
+        );
 
-        auction.creator.transfer(amountToPay);
+        (bool success, ) = auction.creator.call{value: amountToPay}("");
+        require(success, "Transfer failed.");
         emit PaymentToOwner(
             auction.creator,
             amountToPay,
-            auction.currentBidAmount,
+            //  auction.currentBidAmount,
             commissionToPay,
             royaltiesToPay,
             amountToPay + commissionToPay + royaltiesToPay
@@ -364,17 +434,60 @@ contract Engine is Ownable {
 
         accumulatedCommission = accumulatedCommission.add(commissionToPay);
 
+        totalSales = totalSales.add(auction.currentBidAmount);
+
         offer.isAuction = false;
         offer.isOnSale = false;
         offers[auction.assetId] = offer;
     }
 
+    //Call this method
+    function cancelAuctionOfToken(uint256 _tokenId)
+        external
+        onlyOwner
+        nonReentrant
+    {
+        Offer memory offer = offers[_tokenId];
+        // is there is an auction open, we have to give back the last bid amount to the last bidder
+        require(offer.isAuction, "Offer is not an auction");
+        Auction memory auction = auctions[offer.idAuction];
+
+        returnLastBid(offer, auction);
+
+        offer.isAuction = false;
+        offers[_tokenId] = offer;
+    }
+
+    function returnLastBid(Offer memory offer, Auction memory auction)
+        internal
+    {
+        // is there is an auction open, we have to give back the last bid amount to the last bidder
+        require(offer.isAuction, "Offer is not an auction");
+        // #4. Only if there is at least a bid and the bid amount > 0, give it back to last bidder
+        require(
+            auction.currentBidAmount != 0 && auction.bidCount > 0 && auction.currentBidOwner != address(0),
+            "No bids yet"
+        );
+        require(offer.creator != auction.currentBidOwner, "Offer owner cannot retrieve own funds");
+        // return funds to the previuos bidder
+        (bool success3, ) = auction.currentBidOwner.call{
+            value: auction.currentBidAmount
+        }("");
+        require(success3, "Transfer failed.");
+        emit ReturnBidFunds(
+            offer.idAuction,
+            auction.currentBidOwner,
+            auction.currentBidAmount
+        );
+    }
+
     // This method is only callable by the marketplace owner and transfer funds from
-    // the marketplace to the caller. 
+    // the marketplace to the caller.
     // It us used to move the funds from the marketplace to the investors
-    function extractBalance() public onlyOwner {
+    function extractBalance() public nonReentrant onlyOwner {
         address payable me = payable(msg.sender);
-        me.transfer(accumulatedCommission);
+        (bool success, ) = me.call{value: accumulatedCommission}("");
+        require(success, "Transfer failed.");
         accumulatedCommission = 0;
     }
 }
