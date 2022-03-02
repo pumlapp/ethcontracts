0868eff4af9f69d2fa696a18fa79b594cc493e22
nickjjang <nickjang234@gmail.com>
2022-03-03 01:30:57 +0700

    fixed collection system

--- contracts/PumlNFT.sol
+++ contracts/PumlNFT.sol
@@ -17,7 +17,7 @@ contract PumlNFT is ERC721URIStorage {
     }
     mapping(uint256 => TokenData) tokens;
 
-    constructor(string memory _name, string memory _symbol, string memory _des) ERC721(_name, _symbol, _des) {}
+    constructor(string memory _name, string memory _symbol, string memory _description) ERC721(_name, _symbol, _description) {}
 
     function getCreator(uint256 _tokenId) public view returns (address) {
         return tokens[_tokenId].creator;
