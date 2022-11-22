0e98aca08694b30dd4ae03e464d0052e4a1980fc
nickjjang <nickjang234@gmail.com>
2022-11-22 11:33:22 -0600

    updated README

--- truffle-config.js
+++ truffle-config.js
@@ -60,20 +60,20 @@ module.exports = {
       timeoutBlocks: 200,
       skipDryRun: true
     },
-    rinkeby: {
-      provider: () => new HDWalletProvider({
-        mnemonic,
-        providerOrUrl:
-          `https://rinkeby.infura.io/v3/${projectId}`,
-        chainId: 4,
-      }),
+    goerli: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic,
+          providerOrUrl: `https://goerli.infura.io/v3/${projectId}`,
+          chainId: 5
+        }),
       // provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${projectId}`),
-      network_id: 4,       // Rinkeby's id
+      network_id: 5, // Rinkeby's id
       //     gas: 5500000,        // Rinkeby has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true,   // Skip dry run before migrations? (default: false for public nets )
-      gasPrice: 40000000000,  // 20 gwei (in wei) (default: 120 gwei)
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+      gasPrice: 40000000000 // 20 gwei (in wei) (default: 120 gwei)
     },
     ropsten: {
       provider: () => new HDWalletProvider({ mnemonic, providerOrUrl: `https://ropsten.infura.io/v3/${projectId}`, chainId: 3 }),
